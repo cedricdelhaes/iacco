e74ff78c182968ed623356c63f23a93d696a1cac
cedricdelhaes <delhaes.c@gmail.com>
2016-11-22 15:42:57 +0100

    refactorisation du code

--- Chrome/script.js
+++ Chrome/script.js
@@ -7,8 +7,8 @@ document.addEventListener('DOMContentLoaded', function() {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
           //Envoi d'un message de demande de tire au content_script et maj de la popup
           chrome.tabs.sendMessage(tabs[0].id, {action: 'getTitle'}, function(response){updateTitle(response.title)});
-          chrome.tabs.sendMessage(tabs[0].id, {action: 'findLn'}, function(response){updateLn(response.ln)});
-          chrome.tabs.sendMessage(tabs[0].id, {action: 'findToS'}, function(response){updateToS(response.tos)});
+          chrome.tabs.sendMessage(tabs[0].id, {action: 'findLn'}, function(response){update('ln', response)});
+          chrome.tabs.sendMessage(tabs[0].id, {action: 'findToS'}, function(response){update('tos', response)});
         });
     });
 });
@@ -18,24 +18,15 @@ function updateTitle(message){
   document.getElementById('owner').textContent = message;
 }
 
-//Mettre à jour les Mentions légales (Ln Legale Notice)
-function updateLn(message){
-  if(message){
-    document.getElementById('ln').textContent = "Trouvé mais non analysé";
-    //document.getElementById('traffic_ln').src = "traffic_lights_yellow.png"
+//Mettre à jour l'ihm selon la reponse
+function update(id,message){
+  if(message.find == undefined){
+    document.getElementById(id).textContent = "Pas de fonction de recherche associé à la demande";
+  }else if(message.find == 'No regexp'){
+    document.getElementById(id).textContent = "Pas d'expression régulière associé à la demande";
+  }else if (message.find){
+    document.getElementById(id).textContent = "Trouvé mais non analysé";
   }else{
-    document.getElementById('ln').textContent = "Non trouvé";
-    //document.getElementById('traffic_ln').src = "traffic_lights_red.png"
-  }
-}
-
-//Mettre à jour les conditions de ventes générales (ToS Termes of Sales)
-function updateToS(message){
-  if(message){
-    document.getElementById('tos').textContent = "Trouvé mais non analysé";
-    //document.getElementById('traffic_tos').src = "traffic_lights_yellow.png"
-  }else{
-    document.getElementById('tos').textContent = "Non trouvé";
-    //document.getElementById('traffic_tos').src = "traffic_lights_red.png"
+    document.getElementById(id).textContent = "Non trouvé";
   }
 }
